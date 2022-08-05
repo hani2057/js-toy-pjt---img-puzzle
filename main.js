e845aafb66d66faecb5d2ce570284b413d410870
hani2057 <hanikim2057@gmail.com>
2022-08-05 23:09:01 +0900

    Build fundamental html, css

---
+++ main.js
@@ -0,0 +1,10 @@
+const playTime = document.querySelector(".play_time");
+const playBoard = document.querySelector(".tiles");
+const completeMsg = document.querySelector(".complete_msg");
+const startBtn = document.querySelector(".start_btn");
+const cheetBtn = document.querySelector(".cheet_btn");
+
+console.log(playBoard.children);
+playBoard.children.foreach(() => {
+  
+})
